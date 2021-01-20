f8a558439b440635585bd5fc8f33dba81832d616
Rob Lark <62610548+Robert-Lark@users.noreply.github.com>
2021-01-20 12:15:35 -0800

    completed faq section

--- src/Components/FaqSection.js
+++ src/Components/FaqSection.js
@@ -1,11 +1,11 @@
 import React from "react";
 import styled from "styled-components";
-import { StyledAbout } from "../styles";
-import { motion } from "framer-motion";
+import {StyledAbout} from "../styles";
+import {motion} from "framer-motion";
 import Toggle from "./Toggle";
-import { AnimateSharedLayout } from "framer-motion";
-import { scrollReveal } from "../Animation";
-import { useScroll } from "./useScroll";
+import {AnimateSharedLayout} from "framer-motion";
+import {scrollReveal} from "../Animation";
+import {useScroll} from "./useScroll";
 
 const FaqSection = () => {
   const [element, controls] = useScroll();
@@ -17,43 +17,62 @@ const FaqSection = () => {
       initial="hidden"
     >
       <h2>
-        Any Queastions? <span>FAQ</span>
+        Contact <span>Me</span>
       </h2>
       <AnimateSharedLayout>
-        <Toggle title="How Do I Start">
+        <Toggle title="Email">
           <div className="answer">
-            <motion.p variant={scrollReveal}>Lorem ipsum dolor sit amet.</motion.p>
+            <motion.p variant={scrollReveal}>robert.lark87@gmail.com</motion.p>
+          </div>
+        </Toggle>
+        <Toggle title="Social Media">
+          <div className="answer">
             <motion.p variant={scrollReveal}>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut quas
-              ad quaerat, esse placeat recusandae?
+              <div className="list">
+                <a href="https://github.com/Robert-Lark">
+                  <motion.p>Github</motion.p>
+                </a>
+                <a href="https://www.instagram.com/burntskies25/">
+                  <motion.p>Instagram Personal</motion.p>
+                </a>
+                <a href="https://www.instagram.com/quietcalmrecords/">
+                  <motion.p>Instagram Quiet Calm Records</motion.p>
+                </a>
+                <a href="https://twitter.com/RobLark4">
+                  <motion.p>Twitter</motion.p>
+                </a>
+                <a href="https://bandcamp.com/deadfactory">
+                  <motion.p>Bandcamp</motion.p>
+                </a>
+                <a href="https://soundcloud.com/quietcast">
+                  <motion.p>Soundcloud</motion.p>
+                </a>
+                <a href="https://www.discogs.com/user/quietcalm">
+                  <motion.p>Discogs</motion.p>
+                </a>
+              </div>
             </motion.p>
           </div>
         </Toggle>
-        <Toggle title="Daily Schedule">
+        <Toggle title="Photography">
           <div className="answer">
-            <motion.p variant={scrollReveal}>Lorem ipsum dolor sit amet.</motion.p>
             <motion.p variant={scrollReveal}>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut quas
-              ad quaerat, esse placeat recusandae?
+              You can view my Photography here:
             </motion.p>
+            <a href="https://dreamstoday14-blog.tumblr.com/">
+              <motion.p variant={scrollReveal}>DREAMS TODAY</motion.p>
+            </a>
           </div>
         </Toggle>
-        <Toggle title="Different Payment Methods?">
+        <Toggle title="Radio Show">
           <div className="answer">
-            <motion.p variant={scrollReveal}>Lorem ipsum dolor sit amet.</motion.p>
             <motion.p variant={scrollReveal}>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut quas
-              ad quaerat, esse placeat recusandae?
+              Tune into my monthly Vinyl only Radio Show hosted on True Vinyl
+              Alternative here:
             </motion.p>
-          </div>
-        </Toggle>
-        <Toggle title="What Products Do You Offer?">
-          <div className="answer">
-            <motion.p variant={scrollReveal}>Lorem ipsum dolor sit amet.</motion.p>
-            <motion.p variant={scrollReveal}>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut quas
-              ad quaerat, esse placeat recusandae?
-            </motion.p>
+            <a href="https://www.quietcalmrecords.com/quiet-calm-radio">
+              <motion.p variant={scrollReveal}>Quiet Calm Radio</motion.p>
+            </a>
           </div>
         </Toggle>
       </AnimateSharedLayout>
@@ -63,6 +82,7 @@ const FaqSection = () => {
 
 const StyledFaq = styled(StyledAbout)`
   display: block;
+  text-align: center;
   span {
     display: block;
   }
@@ -84,6 +104,18 @@ const StyledFaq = styled(StyledAbout)`
     padding: 2rem 0rem;
     p {
       padding: 1rem 0rem;
+    }
+    a {
+      text-decoration: none;
+    }
+
+    .list {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      a {
+        text-decoration: none;
+      }
     }
   }
 `;
