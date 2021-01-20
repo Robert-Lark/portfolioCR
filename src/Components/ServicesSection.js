d71765c9a3fea64c1e8acb459a3538c8089dfa58
Rob Lark <62610548+Robert-Lark@users.noreply.github.com>
2021-01-20 11:08:53 -0800

    testerr

--- src/Components/ServicesSection.js
+++ src/Components/ServicesSection.js
@@ -105,7 +105,7 @@ const StyledCards = styled.div`
     justify-content: center;
  width: 100vw;
  height: 40vh;
- grid-template-columns: 10% 10% 10% 10%;
+ grid-template-columns: 40% 40%;
   }
   @media (max-width: 900px) {
     grid-template-columns: 100px 100px;
