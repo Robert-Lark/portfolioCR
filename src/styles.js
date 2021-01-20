9d3ed5f17118aca54da81f27ae1f8a7e9be58184
Rob Lark <62610548+Robert-Lark@users.noreply.github.com>
2021-01-20 10:49:54 -0800

    tester2

--- src/styles.js
+++ src/styles.js
@@ -8,6 +8,8 @@ export const StyledAbout = styled(motion.div)`
   justify-content: space-between;
   padding: 5rem 10rem;
   color: white;
+  max-height: 100%;
+    overflow-y: hidden;
   @media (max-width: 1300px) {
     display: block;
     padding: 2rem 2rem;
