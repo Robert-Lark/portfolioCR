330fcc2fa629f05cf315ffc18959df2e90c96ed3
Rob Lark <62610548+Robert-Lark@users.noreply.github.com>
2021-01-24 18:03:33 -0800

    changed tools grid9

--- src/Animation.js
+++ src/Animation.js
@@ -81,10 +81,10 @@ export const SliderContainer = {
 };
 
 export const scrollReveal = {
-  hidden: { opacity: 0, scale: 1.2, transition: { duration: 0.5 } },
+  hidden: { opacity: 0, scale: 1, transition: { duration: 0.5 } },
   show: {
     opacity: 1,
-    scale: 1,
+    scale: 0.8,
     transition: {
       duration: 0.5,
     },
